23bf254b8cec8b68647512f4c2f26ce252797de0
Polina Giralt <polina.giralt@gmail.com>
2013-11-26 17:34:40 -0500

    tests and server code is more modular

--- server.js
+++ server.js
@@ -4,30 +4,37 @@ var fs = require('fs');
 var DAY = "day";
 var NIGHT = "night";
 
+
 var server = {
   requestHandler: function (req, res) {
+    
+    var res_Write = function(ContentType, data){
+      res.writeHead(200, { 'Content-Type': ContentType});
+      res.end(data);
+    };
+
     if (req.url === "/") {
       fs.readFile(__dirname + '/index.html', 'utf8', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
+        res_Write('text/html', data);
       });
     } else if (req.url === "/client.js") {
       fs.readFile(__dirname + '/client.js', 'utf8', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'application/javascript' });
-        res.end(data);
+        res_Write('application/javascript', data);
       });
     } else if (req.url === "/canvas-renderer.js") {
       fs.readFile(__dirname + '/canvas-renderer.js', 'utf8', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'application/javascript' });
-        res.end(data);
+        res_Write('application/javascript', data);
       });
     } else if (req.url === "/time.json") {
       res.writeHead(200, { 'Content-Type': 'application/json' });
-
       var hour = new Date().getHours();
       var time = hour > 6 && hour < 20 ? DAY : NIGHT;
-
       res.end('{ "time": "' + time + '" }');
+    }
+    else {
+      console.log("bad things");
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end("you broke the internet!");
     }
   }
 };
--- tests/server_tests.js
+++ tests/server_tests.js
@@ -16,15 +16,27 @@ var mockRes = function(expectedString, done) {
   return new http.ServerResponse({});
 };
 
+var genericTest = function(urlString, searchString, done) {
+    server.requestHandler({ url: urlString },
+                          mockRes(searchString, done));
+}
+
 // Demo async tests
 tester.test(
   'should serve index.html when go to "/" URL', function(done) {
-    server.requestHandler({ url: "/" },
-                          mockRes("<!doctype", done));
+    genericTest("/", "<!doctype", done);
   },
 
   'should serve client.js when go to "/client.js" URL', function(done) {
-    server.requestHandler({ url: "/client.js" },
-                          mockRes(";(function(exports) {\n  exports.get", done));
+    genericTest("/client.js", ";(function(exports) {\n  exports.get", done);
+  },
+
+  'should serve canvas-renderer.js when go to "/canvas-renderer.js" URL', function(done) {
+    genericTest("/canvas-renderer.js", ";(function(exports) {\n  exports.can", done);
+  },
+
+  'should serve time.json when go to "/time.json" URL', function(done) {
+    genericTest("/time.json", '{ "time":', done);
   }
 );
+
